ead392a75ace1727e2fd0e6ecad9fe0be2002c31
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-06-08 18:48:03 +0530

    Initial Commit

---
+++ website/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/main.css";
+const App = () => {
+  return (
+    <div>
+      <h1>Hello! World From MeraJinn</h1>
+    </div>
+  );
+};
+
+export default App;
