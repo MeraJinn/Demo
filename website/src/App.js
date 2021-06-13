4b21e49876b983e07b0d6e40e22d6d944b8d411d
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-06-13 20:16:28 +0530

    Added Sidebar

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles/main.css";
 import Navbar from "./components/navbar";
+import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router } from "react-router-dom";
 const App = () => {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
