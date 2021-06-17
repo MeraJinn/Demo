c5fe4077d62f52058b9d681d6c9bf974cb4b0e7a
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-06-18 03:52:18 +0530

    Footer and Nav Styling

---
+++ website/src/components/footer/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <FooterContainer>
+        <FooterWrap>
+          <FooterLinksContainer>
+            <FooterLinksWrapper>
+              <FooterLinkItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to="/signin">How it Works?</FooterLink>
+                <FooterLink to="/signin">Help</FooterLink>
+                <FooterLink to="/signin">Your Benefit</FooterLink>
+                <FooterLink to="/signin">Register</FooterLink>
+                <FooterLink to="/signin">Terms of Services</FooterLink>
+              </FooterLinkItems>
+              <FooterLinkItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to="/signin">How it Works?</FooterLink>
+                <FooterLink to="/signin">Help</FooterLink>
+                <FooterLink to="/signin">Your Benefit</FooterLink>
+                <FooterLink to="/signin">Register</FooterLink>
+                <FooterLink to="/signin">Terms of Services</FooterLink>
+              </FooterLinkItems>
+            </FooterLinksWrapper>
+            <FooterLinksWrapper>
+              <FooterLinkItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to="/signin">How it Works?</FooterLink>
+                <FooterLink to="/signin">Help</FooterLink>
+                <FooterLink to="/signin">Your Benefit</FooterLink>
+                <FooterLink to="/signin">Register</FooterLink>
+                <FooterLink to="/signin">Terms of Services</FooterLink>
+              </FooterLinkItems>
+              <FooterLinkItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to="/signin">How it Works?</FooterLink>
+                <FooterLink to="/signin">Help</FooterLink>
+                <FooterLink to="/signin">Your Benefit</FooterLink>
+                <FooterLink to="/signin">Register</FooterLink>
+                <FooterLink to="/signin">Terms of Services</FooterLink>
+              </FooterLinkItems>
+            </FooterLinksWrapper>
+          </FooterLinksContainer>
+        </FooterWrap>
+      </FooterContainer>
+    </>
+  );
+};
+
+export default Footer;
--- website/src/components/navbar/index.js
+++ website/src/components/navbar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Nav,
   NavbarContainer,
@@ -13,8 +13,22 @@ import {
 import { FaBars } from "react-icons/fa";
 
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
   return (
-    <Nav>
+    <Nav scrollNav={scrollNav}>
       <NavbarContainer>
         <NavLogo to="/">Merajinn</NavLogo>
         <MobileIcon onClick={toggle}>
